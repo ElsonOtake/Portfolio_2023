10e0a26521fcdd4ba9a45c9c88ff1734ed80f3c4
Elson Otake <elsonotake@yahoo.com.br>
2023-01-10 15:44:23 -0300

    Remove unused code

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Elson's Portfolio</title>
+      </Head>
+
+      <h1>Let's build an AWESOME portfolio</h1>
+    </>
+  )
+}
