656a5a175c4ed60b3c13ff0441323135d227d513
Elson Otake <elsonotake@yahoo.com.br>
2023-01-10 18:25:50 -0300

    Add call to Hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,31 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Elson's Portfolio</title>
       </Head>
 
-    {/* Header */}
       <Header />
 
-    {/* Hero */}
+      {/* Hero */}
+      <section id='hero' className='snap-center'>
+        <Hero />
+      </section>
 
-    {/* About */}
+      {/* About */}
 
-    {/* Experience */}
+      {/* Experience */}
 
-    {/* Skills */}
+      {/* Skills */}
 
-    {/* Projects */}
+      {/* Projects */}
 
-    {/* Contact Me */}
-    </>
+      {/* Contact Me */}
+    </div>
   )
 }
