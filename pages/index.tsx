f53947b9c3736ba54d81c5c76e7fd0752da228e1
Elson Otake <elsonotake@yahoo.com.br>
2023-01-22 22:11:52 -0300

    Add experience link

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
+import WorkExperience from '../components/WorkExperience'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,17 +14,17 @@ export default function Home() {
 
       <Header />
 
-      {/* Hero */}
       <section id='hero' className='snap-start'>
         <Hero />
       </section>
 
-      {/* About */}
       <section id='about' className='snap-center'>
         <About />
       </section>
 
-      {/* Experience */}
+      <section id='experience' className='snap-center'>
+        <WorkExperience />
+      </section>
 
       {/* Skills */}
 
