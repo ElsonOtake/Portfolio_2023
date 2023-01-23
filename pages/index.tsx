ff5a5e5f9b6a2a9691d21c47b960e2a882a87a50
Elson Otake <elsonotake@yahoo.com.br>
2023-01-22 21:10:39 -0300

    Add index tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,11 +14,14 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id='hero' className='snap-center'>
+      <section id='hero' className='snap-start'>
         <Hero />
       </section>
 
       {/* About */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
 
       {/* Experience */}
 
