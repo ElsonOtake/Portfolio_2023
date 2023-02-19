12eeb9022c1c3b2d6c627b92cf9096766f1a88ce
Elson Otake <elsonotake@yahoo.com.br>
2023-02-19 20:24:17 -0300

    Add email template

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -14,7 +14,10 @@ type Props = {};
 function ContactMe({}: Props) {
   const { register, handleSubmit } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:elsonotake@gmail.com?subject=${formData.subject}&
+    body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+  };
   
   return (
     <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
